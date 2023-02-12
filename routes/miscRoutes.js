3f63377504b91ea7cf97307fb351382916fcfe44
TukanDev <contact@tukandev.com>
2023-02-12 06:51:11 +0100

    small changes, cleanups...
    Signed-off-by: TukanDev <contact@tukandev.com>

--- routes/miscRoutes.js
+++ routes/miscRoutes.js
@@ -1,27 +1,20 @@
 const express = require('express')
-const {ActionType} = require('../utils/constants')
-const {params} = require("superagent/lib/utils");
 
 module.exports = (function() {
     let miscr = express.Router()
 
     // hk4e-sdk-os.hoyoverse.com
-    miscr.get(`/hk4e_global/mdk/agreement/api/getAgreementInfos`, function (req, res) {
+    miscr.get(`/:platform/mdk/agreement/api/getAgreementInfos`, function (req, res) {
         return res.json({retcode: 0, message: "OK", data: {marketing_agreements:[]}
         })
     })
 
     // hk4e-sdk-os.hoyoverse.com
-    miscr.all(`/hk4e_global/combo/granter/api/compareProtocolVersion`, function (req, res) {
+    miscr.all(`/:platform/combo/granter/api/compareProtocolVersion`, function (req, res) {
         return res.json({"retcode":0,"message":"OK","data":{"modified":true,"protocol":{"id":0,"app_id":4,"language":"en","user_proto":"","priv_proto":"","major":7,"minimum":0,"create_time":"0","teenager_proto":"","third_proto":""}}})
     })
 
-    // api-account-os.hoyoverse.com
-    /*miscr.all(`/account/risky/api/check`, function (req, res) {
-        return res.json({retcode:0,message:"OK",data:{id:"none",action:ActionType.NONE,geetest:null}})
-    })*/
-
-    miscr.all(`/common/h5log/log/batch?topic=plat_explog_sdk_v2`, function (req, res) {
+    miscr.all(`/common/h5log/log/batch`, function (req, res) {
         return res.json({"retcode":0,"message":"OK","data":{}})
 
     })
@@ -29,15 +22,6 @@ module.exports = (function() {
     // sdk-os-static.hoyoverse.com
     miscr.all(`/combo/box/api/config/sdk/combo`, function (req, res) {
         return res.json({retcode:0,message:"OK","data":{vals:{disable_email_bind_skip:false,email_bind_remind_interval:"7",email_bind_remind:true}}})
-    })
-
-    // hk4e-sdk-os-static.hoyoverse.com
-    miscr.get(`/hk4e_global/combo/granter/api/getConfig`, function (req, res) {
-        return res.json({"retcode":0,"message":"OK","data":{"protocol":true,"qr_enabled":false,"log_level":"INFO","announce_url":"https://webstatic-sea.hoyoverse.com/hk4e/announcement/index.html?sdk_presentation_style=fullscreenu0026sdk_screen_transparent=trueu0026game_biz=hk4e_globalu0026auth_appid=announcementu0026game=hk4e#/","push_alias_type":2,"disable_ysdk_guard":false,"enable_announce_pic_popup":true}})
-    })
-
-    miscr.get(`/hk4e_global/mdk/shield/api/loadConfig`, function (req, res) {
-        return res.json({"retcode":0,"message":"OK","data":{"id":6,"game_key":"hk4e_global","client":"PC","identity":"I_IDENTITY","guest":false,"ignore_versions":"","scene":"S_NORMAL","name":"原神海外","disable_regist":false,"enable_email_captcha":false,"thirdparty":["fb","tw"],"disable_mmt":false,"server_guest":false,"thirdparty_ignore":{"tw":"","fb":""},"enable_ps_bind_account":false,"thirdparty_login_configs":{"tw":{"token_type":"TK_GAME_TOKEN","game_token_expires_in":604800},"fb":{"token_type":"TK_GAME_TOKEN","game_token_expires_in":604800}}}})
     })
 
     // abtest-api-data-sg.hoyoverse.com
@@ -71,6 +55,12 @@ module.exports = (function() {
         return res.json({code: 0})
     })
 
+    // /client/event/dataUpload
+    miscr.post(`/client/event/dataUpload`, function (req, res) {
+        //console.log(`/client/event/dataUpload`, req.body)
+        return res.json({code: 0})
+    })
+
     // /perf/config/verify?device_id=xxx&platform=x&name=xxx
     miscr.all(`/perf/config/verify`, function (req, res) {
         return res.json({code: 0})
@@ -80,7 +70,6 @@ module.exports = (function() {
     miscr.get(`/admin/mi18n/plat_oversea/*`, function (req, res) {
         return res.send(`your mom`)
     })
-
 
     miscr.all(`/device-fp/api/getExtList`, function (req, res) {
         return res.send({"retcode":0,"message":"OK","data":{"code":200,"msg":"ok","ext_list":["cpuName","systemName","systemType","deviceUID","gpuID","gpuName","gpuAPI","gpuVendor","gpuVersion","gpuMemory","osVersion","cpuCores","cpuFrequency","gpuVendorID","isGpuMultiTread","memorySize"],"pkg_list":[]}})
